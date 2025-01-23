46da2e62abbdeda694564285398056960a12bbbd
yjeong <95263915+woyuje@users.noreply.github.com>
2025-01-23 21:31:23 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -0,0 +1,12 @@
+def solution(angle):
+    if 0<angle<90 :
+        answer=1
+    elif angle==90:
+        answer=2
+    elif 90<angle<180:
+        answer=3
+    elif angle==180:
+        answer=4
+    else :
+        print("예시가 없습니다.")
+    return answer
