b80b327c7539de860ea633e0f0108d6d93467252
yjeong <95263915+woyuje@users.noreply.github.com>
2025-02-04 10:33:15 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,10 @@
+def solution(price):
+    if price >=500000:
+        return int(price*0.8)
+    elif price>=300000 and price<500000:
+        return int(price*0.9)
+    elif   price>=100000 and price<300000:
+        return int(price*0.95)
+    else:
+        return price
+    
